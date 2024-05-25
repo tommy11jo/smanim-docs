7d430e533b5ae667ab008026a439f1d2ae493248
tom11my <tommy11jo@gmail.com>
2024-05-24 22:48:03 -0700

    - Add docs for 	- labeling 	- surrounding-rectangle 	- close-to

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type { Config } from "@docusaurus/types"
 import type * as Preset from "@docusaurus/preset-classic"
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Still Manim Docs",
+  tagline: "Coding Diagrams",
   favicon: "img/logo.svg",
 
   // Set the production url of your site here
